e57ec5a00ecce7a53c9b0ba87e869e9d3bb15e6d
jessicakwalters <jessicakwalters@gmail.com>
2019-09-04 09:14:00 -0700

    add events and logging

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 'use strict';
 
 const fs = require('fs');
+const events = require('./events.js');
+require('./cache.js');
+require('./logger.js');
+
+events.emit('save', { id: 101 });
+events.emit('delete', { id: 101 });
 
 const alterFile = (file) => {
   fs.readFile( file, (err, data) => {
