c641da4a4eed0aa6bed818dda231fb155bd2023d
jessicakwalters <jessicakwalters@gmail.com>
2019-09-04 09:29:51 -0700

    refactor files

--- app.js
+++ app.js
@@ -1,46 +1,20 @@
 'use strict';
 
-const fs = require('fs');
 const events = require('./events.js');
+
 require('./cache.js');
 require('./logger.js');
 
-events.emit('save', { id: 101 });
-events.emit('delete', { id: 101 });
-
-const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
-    });
-  });
-};
-
-const readFile = (file) => {
-  fs.readFile(file, (err, data) => {
-    if (err) {
-      throw err;
-    }
-    return data;
-  });
-};
-
-const upperCase = (data) => {
-  let text = data.toString().toUpperCase();
-  return text;
-};
-
-const writeFile = (text) => {
-  fs.writeFile( file, Buffer.from(text), (err, data) => {
-    if (err) {
-      throw err;
-    }
-    console.log(`${file} saved`);
-  })
-};
+// const alterFile = (file) => {
+//   fs.readFile( file, (err, data) => {
+//     if(err) { throw err; }
+//     let text = data.toString().toUpperCase();
+//     fs.writeFile( file, Buffer.from(text), (err, data) => {
+//       if(err) { throw err; }
+//       console.log(`${file} saved`);
+//     });
+//   });
+// };
 
 let file = process.argv.slice(2).shift();
 //alterFile(file);
@@ -48,3 +22,7 @@ let file = process.argv.slice(2).shift();
 let data = readFile(file);
 let text = upperCase(data);
 writeFile(text);
+
+events.emit('read', { file: file });
+events.emit('change case', { file: file });
+events.emit('write', { file: file });
