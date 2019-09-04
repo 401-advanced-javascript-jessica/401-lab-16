cfd85c496a1b53a86d9926728f2af161a447dff4
jessicakwalters <jessicakwalters@gmail.com>
2019-09-04 08:58:42 -0700

    breakup alterfile function

--- app.js
+++ app.js
@@ -13,5 +13,32 @@ const alterFile = (file) => {
   });
 };
 
+const readFile = (file) => {
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    return data;
+  });
+};
+
+const upperCase = (data) => {
+  let text = data.toString().toUpperCase();
+  return text;
+};
+
+const writeFile = (text) => {
+  fs.writeFile( file, Buffer.from(text), (err, data) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`${file} saved`);
+  })
+};
+
 let file = process.argv.slice(2).shift();
-alterFile(file);
+//alterFile(file);
+
+let data = readFile(file);
+let text = upperCase(data);
+writeFile(text);
